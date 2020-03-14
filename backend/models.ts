a837b3f423b9472a3e92326ee8ab028674a7ed5d
ben@dcdc.io <ben@dcdc.io>
2020-03-14 02:23:21 +0000

    tests

--- backend/models.ts
+++ backend/models.ts
@@ -1,11 +1,20 @@
-import { prop, getModelForClass } from '@typegoose/typegoose'
+import { prop, getModelForClass, pre } from '@typegoose/typegoose'
 import mongoose from 'mongoose'
 
 export enum Defaults {
     UnassignedJob = "UnassignedJob"
 }
 
-export class Person {
+@pre<Modifiable>('save', function() { })
+export class Logged { }
+
+@pre<Modifiable>('save', function() { if (this.created) { this.modified = Date.now() } else this.created = Date.now() })
+export class Modifiable extends Logged {
+    @prop() public created!: number
+    @prop() public modified!: number
+}
+
+export class Person extends Modifiable {
     @prop({ required: true }) public name!: string
     @prop({ required: true }) public address!: string
     @prop({ required: true }) public phone!: string
@@ -19,12 +28,11 @@ export class Recipient extends Person {
     @prop() public email!: string
 }
 
-export class Job {
+export class Job extends Modifiable {
     @prop() public workerId?: string = Defaults.UnassignedJob
     @prop({ required: true }) public recipientId!: string 
-    @prop() public description!: string 
+    @prop() public description!: string
 }
-
 export class PointToPointJob extends Job {
     @prop({ required: true }) public pickupAddress! : string
     @prop({ required: true }) public dropOffAddress! : string
