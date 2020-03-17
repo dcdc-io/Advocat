e77def31f8185da785e79744de05e6497a190434
jamesT@saglobal.com <james@dcdc.io>
2020-03-17 03:07:53 +0000

    workers

---
+++ pwa/pwa/src/routes/workers.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { useDatabase } from '../helpers.js'
+    const workers = useDatabase("workers")
+    
+    let docs = await workers.allDocs()
+</script>
+
+<div>workers</div>
+{#each docs as doc}
+<p>a worker</p>
+{/each}
