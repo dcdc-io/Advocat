136537721675f87f950193cece812f7c2a3ba9c9
ben@dcdc.io <ben@dcdc.io>
2020-03-17 15:23:51 +0000

    front page

--- pwa/pwa/src/routes/index.svelte
+++ pwa/pwa/src/routes/index.svelte
@@ -1,7 +1,23 @@
 <script>
   import { getContext } from "svelte";
+  import { goto } from '@sapper/app'
   let { loggedIn, username } = getContext("user");
+
+  const click = (page) => () => goto(page)
 </script>
+
+<style lang="scss">
+  .calltoaction {
+    background: black;
+    color: white;
+    padding: 1em;
+    margin: 1em 0;
+    cursor: pointer;
+    h2 {
+      text-decoration: underline;
+    }
+  }
+</style>
 
 {#if $loggedIn}
   <div>
@@ -9,6 +25,17 @@
   </div>
 {:else}
   <div>
-    <span>welcome to advocat.</span>
+    <div>
+      <h1>welcome to advocat.</h1>
+      <p>advocat. is a social network for the real world</p>
+    </div>
+    <div class="calltoaction" on:click={click("findgroups")}>
+      <h2>find groups</h2>
+      <p>find groups in your local area</p>
+    </div>
+    <div class="calltoaction" on:click={click("createagroup")}>
+      <h2>create a group</h2>
+      <p>create a group with other people in your area</p>
+    </div>
   </div>
 {/if}
