9bf600accefba0908cd0a9de0ef7f6dc9f3017b0
ben@dcdc.io <ben@dcdc.io>
2020-03-18 20:33:21 +0000

    tidying

--- slouchdb/index.js
+++ slouchdb/index.js
@@ -1,37 +1,11 @@
 const fuxor = require('fuxor')
 fuxor.add('pouchdb-security', require('slouchdb-security'))
+const PouchDB = require('pouchdb')
 
-const PouchDB = require('pouchdb').plugin(require('slouchdb-security'))
-
-
-// testing permission stuff
-let _users = new PouchDB("_users")
-_users.allDocs().then(result => console.log(result))
-
-_users.getSecurity()
-    .then(security => { 
-            console.log(security)
-        return _users.putSecurity({
-            "members": [
-                "group1", "group2"
-            ],
-            "readers": [
-                "group3"
-            ],
-            "writers": [
-                "group4"
-            ]
-        })
-    })
-    .catch(error => {
-        console.log(error)
-    })
-
-var app = require('express-pouchdb')(PouchDB, {
+const app = require('express-pouchdb')(PouchDB, {
     mode: 'fullCouchDB',
     overrideMode: {
       include: ['routes/fauxton']
     }
   });
-  app.setPouchDB(require('pouchdb'));
-  app.listen(5984)
+app.listen(5984)
