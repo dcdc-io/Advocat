e45407ddcf06bc99501005fe5557c93a2e9fbc01
ben@dcdc.io <ben@dcdc.io>
2020-03-18 19:14:17 +0000

    removed old dbs

--- slouchdb/index.js
+++ slouchdb/index.js
@@ -3,6 +3,8 @@ fuxor.add('pouchdb-security', require('slouchdb-security'))
 
 const PouchDB = require('pouchdb').plugin(require('slouchdb-security'))
 
+
+// testing permission stuff
 let _users = new PouchDB("_users")
 _users.allDocs().then(result => console.log(result))
 
@@ -28,20 +30,8 @@ _users.getSecurity()
 var app = require('express-pouchdb')(PouchDB, {
     mode: 'fullCouchDB',
     overrideMode: {
-      include: ['routes/fauxton'],
-     /* exclude: [
-        'routes/authentication',
-        // disabling the above, gives error messages which require you to disable the
-        // following parts too. Which makes sense since they depend on it.
-        'routes/authorization',
-        'routes/session'
-      ] */
+      include: ['routes/fauxton']
     }
   });
-  // when not specifying PouchDB as an argument to the main function, you
-  // need to specify it like this before requests are routed to ``app``
   app.setPouchDB(require('pouchdb'));
-
-  
-
   app.listen(5984)
