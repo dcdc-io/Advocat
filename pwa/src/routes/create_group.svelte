dc79d1efe006174228249478f2aedaae1f33e978
David Charnock <thegr8dave@gmail.com>
2020-03-20 16:53:23 +0000

    finally got create group doing something

--- pwa/src/routes/create_group.svelte
+++ pwa/src/routes/create_group.svelte
@@ -7,23 +7,29 @@
   import { useDatabase } from '../helpers.js'
   import { Form, Input, Select, Choice } from 'sveltejs-forms'
   import * as yup from 'yup'
+  import { v4 as uuidv4 } from 'uuid';
   let schema
   let initialValues
-  let handleSubmit
-  (async () => {
-    const groups = useDatabase({name:"groups"})
+
+  const groups = useDatabase({name:"groups"})
 
 
-    schema = yup.object().shape({
-      group: yup.object().shape({
-        location: yup.string().required()
-      }),
-    })
-    initialValues = {}
-    handleSubmit = async ({detail:{values,setSubmitting,resetForm}}) => {
-      (await groups).put({"Location": groups.location})
-    }
-  } )()
+  schema = yup.object().shape({
+    group: yup.object().shape({
+      location: yup.string().required()
+    }),
+  })
+  initialValues = {}
+
+
+
+  const handleSubmit = ({detail:{values,setSubmitting,resetForm}}) => { 
+    groups.put({_id: uuidv4(), location: "leeds"}).then((result) =>
+    {
+      console.log(result)
+      setSubmitting(false)      
+    }).catch(error => console.log(error))  
+  }
 </script>
 
 <div>create advocat group</div>
