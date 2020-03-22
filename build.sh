7dbd9b46c738a452d8c9077ef815da5fbb8bf405
ben@dcdc.io <ben@dcdc.io>
2020-03-22 02:12:51 +0000

    build script

--- build.sh
+++ build.sh
@@ -9,13 +9,11 @@ echo $PWD
 (cd pouchdb-security && yarn)
 
 # build couchdb-fauxton
-# (cd couchdb-fauxton-1.1.10 && yarn && yarn couchdb)
-# ^^^ not need as pouchdb-fauxton will build
-
-# build pouchdb-fauxton
-(cd pouchdb-fauxton && yarn)
+(cd couchdb-fauxton-1.2.3 && yarn && yarn couchdb)
 
 ## no longer used
+## # build pouchdb-fauxton
+## (cd pouchdb-fauxton && yarn)
 ## # build backoffice pouchdb-server
 ## (cp -r pouchdb-fauxton/www server/www && yarn)
 
@@ -23,4 +21,4 @@ echo $PWD
 (cd pwa && yarn && yarn build)
 
 # copy fauxton build to pwa
-(cp -r pouchdb-fauxton/www pwa/__sapper__/build/_utils)
+(cp -r couchdb-fauxton-1.2.3/dist/release pwa/__sapper__/build/_utils)
