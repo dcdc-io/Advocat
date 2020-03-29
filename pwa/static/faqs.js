b693f7c73b5857cc047a6d7d682fc31a151f64be
Rhys Kyte <rskyte@gmail.com>
2020-03-29 03:57:23 +0100

    most faqs edited

--- pwa/static/faqs.js
+++ pwa/static/faqs.js
@@ -33,7 +33,8 @@ const faqs = [
         ]
     },
     {
-        question: "How can I help people who have no internet?",
+        // EDIT
+        question: "How can I help people who don't have internet access?",
         answer: [
             `If you have neighbours that have no internet you can write and leave a little note for them, asking them to write down if they need anything.
             If you trust them you can leave them your phone number so they can call you and explain their needs, you can post their request on the advocat. website so other voulnteers can help them too.`
@@ -42,19 +43,20 @@ const faqs = [
     {
         question: "How do I handover items to quaranteened people?",
         answer: [
-            `Make sure there is no contact between a person in quarentine and a volunteer. 
-            We recommend you leave the delivered items in front of the door, exchange a phone number and call a person to let them know about delivery. 
-            If its an old person that does not use a digital banking then ask them to put the money in the envelope and slide it down the door.`
+            `Make sure to fully follow the self-isolation/quarantine guidelines set out by your local authority (re "How do I keep myself and the people that I'm helping safe?"). 
+            We recommend you leave the delivered items in front of the door, and notify the receiver of their delivery.`
         ]
     },
     {
         question: "How do I pay for purchases?",
         answer: [
-            `It is your own responsibility to find best way of handling payments. We recommend to have minimum direct contact with people and not to use cash. 
-            We encourage digital money transfer like PayPal or via your banking app.`
+            `Handling of payment is the responsibility of the volunteer and the person receiving items; using a method that both parties are comfortable with.
+            As we are trying to minimise human contact, we don't recommend using cash. 
+            instead we suggest using online banking apps or digital money transfer apps such as PayPal.`
         ]
     },
-    {
+    {   
+        // EDIT
         question: "What if a person does not pay for a purchase?",
         answer: [
             `advocat. cannot accept any liability for requests made or monies transferred. 
@@ -65,15 +67,15 @@ const faqs = [
     {
         question:"Who can see my requests?",
         answer: [
-            `Anyone who visits advocat. can see your requests and an approximate location, 
-            but rest assured only registered and verified volunteers can respond to your requests.`
+            `Anyone who uses advocat. can see your requests and an approximate location, 
+            but rest assured only registered and verified volunteers can respond to your requests and see their details in full.`
         ]
     },
     {
         question: "How is my data used?",
         answer: [
-            `Your email address and personal details will not be visible to public. All data is safely stored in dcdc database and compliant with our <a href="data" target="_blank">data policy</a>.
-            We might use locations for statistical analysis and predictions of a virus spread and breakouts, this might help volunteers organise better and put more workforse where needed.`
+            `Your email address and personal details will not be publically visible. All data is safely stored in databases owned and run by <a href="https://dcdc.io">DCDC</a> and is compliant with our <a href="data" target="_blank">data policy</a>.
+            We may use location data for statistical analysis in order to better organise volunteers and provide help where needed faster and more robustly.`
         ]
     }
 ]
