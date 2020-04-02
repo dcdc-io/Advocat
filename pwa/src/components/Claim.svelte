3b4725ac836ce8595866e6d4b801e6df5881fb5b
jamesT@saglobal.com <james@dcdc.io>
2020-04-02 18:04:37 +0100

    claim creation field  sorting

--- pwa/src/components/Claim.svelte
+++ pwa/src/components/Claim.svelte
@@ -1,6 +1,10 @@
 <script>
     import { getContext } from 'svelte';
     export let claim;
+    import Edit from "svelte-material-icons/briefcaseEditOutline.svelte";
+    import Delete from "svelte-material-icons/briefcaseRemoveOutline.svelte";
+
+
 </script>
 
 <style>
@@ -11,17 +15,17 @@
 </style>
 
 <!-- TODO: if signed this should have a QR code so we can prove the signature on a trusted device -->
-<!-- TODO: this should also include Edit/delete buttons -->
 
 <div class="claim-container" id="claim.formName">
     {#if claim}
-        <h5> {claim.formName} - version {claim.formVersion}</h5>
+        <h5> {claim.formName} - version {claim.formVersion}  <Edit size=4em ></Edit> </h5> 
         {#each claim.fields.sort( (a,b) => a.order - b.order) as data}
             <div class="claim-field">
                 <label class="claim-field-name">{data.name}:</label>
                 <span class="claim-field-data">{data.value}</span>
             </div>
         {/each}      
+         <Edit size=3em ></Edit><Delete size=3em ></Delete>
     {:else}
         no claim being passed
     {/if}
