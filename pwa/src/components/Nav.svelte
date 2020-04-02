d34397c4707accdc5d744956b5f93d0c1c5dbfe9
jamesT@saglobal.com <james@dcdc.io>
2020-04-02 12:13:13 +0100

    wip

--- pwa/src/components/Nav.svelte
+++ pwa/src/components/Nav.svelte
@@ -1,59 +1,59 @@
 <script>
-	import { stores } from '@sapper/app';
-	import { getContext } from 'svelte';
-	import { Tabs } from '../../node_modules/smelte/src';
+    import { stores } from '@sapper/app';
+    import { getContext } from 'svelte';
+    import { Tabs } from '../../node_modules/smelte/src';
 
     const { page } = stores();
-	let { loggedIn, username } = getContext("user")
-	let accountMenu = [];
+    let { loggedIn, username } = getContext("user")
+    let accountMenu = [];
 
-	const navMenu = [
-		{ to: '/', text: 'Home'},
-		{ to: '/registration_map', text: 'Registration Map'}
-	]
+    const navMenu = [
+        { to: '/', text: 'Home'},
+        { to: '/registration_map', text: 'Registration Map'}
+    ]
 
-	$: path = $page.path;
-	$: {
-		if ($loggedIn) {
-			accountMenu = [
-				{ to: '/claims', text: 'claims'},
-				{ to: '/account', text: $username },
-				{ to: '/signout', text: 'Logout' }
-			]
-		} else {
-			accountMenu = [
-				{ to: '/login', text: 'Login' },
-				{ to: '/register', text: 'Register' }
-			]
-		}
-	}
+    $: path = $page.path;
+    $: {
+        if ($loggedIn) {
+            accountMenu = [
+                { to: '/claims', text: 'claims'},
+                { to: '/account', text: $username },
+                { to: '/signout', text: 'Logout' }
+            ]
+        } else {
+            accountMenu = [
+                { to: '/login', text: 'Login' },
+                { to: '/register', text: 'Register' }
+            ]
+        }
+    }
 </script>
 
 <style>
-	.nav-box {
-		background: var(--colour-scheme-dark);
-	}
-	.rightmenu { 
-		width: 200px;
-		float: right;
-		overflow: auto; 
-	}
-	.leftmenu { 
-		width: 250px;
-		float: left;
-		overflow: auto; 
-		white-space: nowrap;
-	}
+    .nav-box {
+        background: var(--colour-scheme-dark);
+    }
+    .rightmenu { 
+        width: 200px;
+        float: right;
+        overflow: auto; 
+    }
+    .leftmenu { 
+        width: 250px;
+        float: left;
+        overflow: auto; 
+        white-space: nowrap;
+    }
 </style>
 <div class="nav-box">
-	<div class="rightmenu">
-		<Tabs
-			items={accountMenu}
-			bind:selected={path} />
-	</div>
-	<div class="leftmenu">
-		<Tabs
-			items={navMenu}
-			bind:selected={path} />
-	</div>
+    <div class="leftmenu">
+        <Tabs
+            items={navMenu}
+            bind:selected={path} />
+    </div>
+    <div class="rightmenu">
+        <Tabs
+            items={accountMenu}
+            bind:selected={path} />
+    </div>
 </div>
