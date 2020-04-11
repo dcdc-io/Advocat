96ded2f77980188c250786404d6a2669fd5a04ca
Rhys Kyte <rskyte@gmail.com>
2020-04-11 02:12:13 +0100

    activities db stuff
    Co-authored-by: Benjamin Babik <ben@curlyben.com>
    Co-authored-by: mail.jt@gmail.com <mail.jt@gmail.com>

--- deploy/2020-04-10-0001.js
+++ deploy/2020-04-10-0001.js
@@ -1,92 +1,182 @@
 require("./_migrate")("2020-04-10-0001", async ({ createDatabase, useDatabase }) => {
 
-    /**************************************************/
-    /*             START OF DEPLOYMENT                */
-    /**************************************************/
+  /**************************************************/
+  /*             START OF DEPLOYMENT                */
+  /**************************************************/
 
-    const job_templates = await createDatabase("job_templates")
-    const job_index = await createDatabase("job_index")
+  const job_templates = await createDatabase("job_templates")
+  const job_index = await createDatabase("job_index")
 
-    await job_templates.putSecurity({ 
-        admins: { roles: ["admin"] }, 
-        readers: { roles: ["_public"] }
-    })
-    await job_index.putSecurity({ 
-        writers: { roles: ["_public"] }, 
-        readers: { roles: ["_public"] }
-    })
+  await job_templates.putSecurity({
+    readers: { roles: ["_public"] }
+  })
+  await job_index.putSecurity({
+    writers: { roles: ["_public"] },
+    readers: { roles: ["_public"] }
+  })
 
-    job_templates.put({ "_id": "void-uk-shopping",   
-                        "void": true,
-                        "icon": "shoppingCart",
-                        "name": "Shopping Cart Demo",
-                        "fields": [
-                          {
-                            "label": "What you want",
-                            "name": "Order",
-                            "default": "",
-                            "inputType": "TextField",
-                            "validation": [["string"], ["required"]],
-                            "order": 10
-                          }
-                        ],
-                        "version": 1
-                    })
+  job_templates.put({
+    "_id": "void-uk-shopping",
+    "void": true,
+    "icon": "shopping_cart",
+    "name": "Shopping Cart Demo",
+    "fields": [
+      {
+        "label": "What you want",
+        "name": "Order",
+        "default": "",
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 10
+      }
+    ],
+    "version": 1
+  })
 
-  
-  job_templates.put({ "_id": "job-header",   
-                    "void": true,
-                    "name": false,
-                    "fields": [
-                      {
-                        "label": "Name",
-                        "name": "Name",
-                        "default": {"custom": "account.name"},
-                        "inputType": "TextField",
-                        "validation": [["string"], ["required"]],
-                        "order": 10
-                      },
-                      {
-                        "label": "Address Line 1",
-                        "name": "addressLine1",
-                        "default": {"custom": "account.addressLine1"},
-                        "inputType": "TextField",
-                        "validation": [["string"], ["required"]],
-                        "order": 20
-                      },
-                      {
-                        "label": "Address Line 2",
-                        "name": "addressLine2",
-                        "default": {"custom": "account.addressLine2"},
-                        "inputType": "TextField",
-                        "validation": [["string"], ["required"]],
-                        "order": 30
-                      },
-                      {
-                        "label": "Town/City",
-                        "name": "townCity",
-                        "default": {"custom": "account.city"},
-                        "inputType": "TextField",
-                        "validation": [["string"], ["required"]],
-                        "order": 40
-                      },
-                      {
-                        "label": "Post/Zip Code",
-                        "name": "postZipCode",
-                        "default": {"custom": "account.postcode"},
-                        "inputType": "TextField",
-                        "validation": [["string"], ["required"]],
-                        "order": 50
-                      },
-                      {
-                        "label": "Phone Number",
-                        "name": "phoneNumber",
-                        "default": {"custom": "account.phoneNumber"},
-                        "inputType": "TextField",
-                        "validation": [["string"], ["required"]],
-                        "order": 60
-                      }    
-                    ],
-                    "version": 1
-                })
+  job_templates.put({
+    "_id": "void-uk-dog-walking",
+    "void": true,
+    "icon": "pets",
+    "name": "Dog Walking",
+    "fields": [
+      {
+        "label": "What you want",
+        "name": "Order",
+        "default": "",
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 10
+      }
+    ],
+    "version": 1
+  })
+
+  job_templates.put({
+    "_id": "void-uk-verification",
+    "void": true,
+    "icon": "verified_user",
+    "name": "ID Verification",
+    "fields": [
+      {
+        "label": "What you want",
+        "name": "Order",
+        "default": "",
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 10
+      }
+    ],
+    "version": 1
+  })
+
+  job_templates.put({
+    "_id": "void-uk-childcare",
+    "void": true,
+    "icon": "child_friendly",
+    "name": "Child Care",
+    "fields": [
+      {
+        "label": "What you want",
+        "name": "Order",
+        "default": "",
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 10
+      }
+    ],
+    "version": 1
+  })
+
+  job_templates.put({
+    "_id": "void-uk-taxi",
+    "void": true,
+    "icon": "local_taxi",
+    "name": "Taxi",
+    "fields": [
+      {
+        "label": "What you want",
+        "name": "Order",
+        "default": "",
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 10
+      }
+    ],
+    "version": 1
+  })
+
+  job_templates.put({
+    "_id": "void-uk-diy",
+    "void": true,
+    "icon": "build",
+    "name": "DIY",
+    "fields": [
+      {
+        "label": "What you want",
+        "name": "Order",
+        "default": "",
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 10
+      }
+    ],
+    "version": 1
+  })
+
+  job_templates.put({
+    "_id": "base-job",
+    "void": true,
+    "name": false,
+    "fields": [
+      {
+        "label": "Name",
+        "name": "Name",
+        "default": { "custom": "account.name" },
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 10
+      },
+      {
+        "label": "Address Line 1",
+        "name": "addressLine1",
+        "default": { "custom": "account.addressLine1" },
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 20
+      },
+      {
+        "label": "Address Line 2",
+        "name": "addressLine2",
+        "default": { "custom": "account.addressLine2" },
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 30
+      },
+      {
+        "label": "Town/City",
+        "name": "townCity",
+        "default": { "custom": "account.city" },
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 40
+      },
+      {
+        "label": "Post/Zip Code",
+        "name": "postZipCode",
+        "default": { "custom": "account.postcode" },
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 50
+      },
+      {
+        "label": "Phone Number",
+        "name": "phoneNumber",
+        "default": { "custom": "account.phoneNumber" },
+        "inputType": "TextField",
+        "validation": [["string"], ["required"]],
+        "order": 60
+      }
+    ],
+    "version": 1
+  })
 })
--- pwa/src/routes/make-activities.svelte
+++ pwa/src/routes/make-activities.svelte
@@ -1,7 +1,11 @@
 <script>
     import { Button, notifier, Notifications} from '../../node_modules/smelte/src'
     import VerticalDrawer from '../components/VerticalDrawer/VerticalDrawer.svelte'
-    import TemplateForm from '../components/templateform.svelte'
+    import TemplateForm from '../components/TemplateForm.svelte'
+
+    let formShapes = {}
+    let db
+    let baseForm;
 
     const button_form = () => {activeForm = true;console.log("test")}
 
@@ -11,17 +15,25 @@
     }
     const completed = (event) => {   
         let doc = event.detail
+        console.log(doc)
         //TODO : jobstuff
         activeForm = false;
         notifier.notify("completed form submission")
     }
 
+    const init = async () => {
+        db = await useDatabase({name: "job_templates"})
+        baseForm = db.get("job-header")
+        formShapes["shopping"] = db.get("void-uk-shopping")
+    }
+
+    onMount( () => {init()})
     let activeForm = false;
 </script>
 
 <div class="flex mb-4 -mx-4">
     <!-- todo: make this build the form using db + account info -->
-    <Button class="bg-gray-500 mx-2 h-12 w-1/3" on:click={() => activeForm = "shopping"}> shopping</Button> 
+    <Button class="bg-gray-500 mx-2 h-12 w-1/3" on:click={() => {activeForm = formShapes["shopping"]}}> shopping</Button> 
     <Button class="bg-gray-500 mx-2 h-12 w-1/3" on:click={() =>{}}> button two</Button>
     <Button class="bg-gray-500 mx-2 h-12 w-1/3" on:click={() =>{}}> button three</Button>
 </div> 
@@ -29,9 +41,17 @@
 <VerticalDrawer bottom={true} persistent={false} bind:show={activeForm}>
     <div style="margin: 2em;">
         {#if activeForm}
-            <TemplateForm  on:cancel={cancelled} on:completed={completed} type="job" template={["job-header", activeForm] } ></TemplateForm>
+            <TemplateForm  on:cancel={cancelled} on:completed={completed} template={activeForm} subtemplate={[baseForm]} ></TemplateForm>
         {/if}
     </div>
 </VerticalDrawer>
 
 <Notifications />
+
+
+
+
+
+
+
+
